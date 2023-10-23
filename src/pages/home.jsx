6ed16c97ea55c8fc20b3e8b7efe1fafe83c31585
kirran01 <Kirran-@live.com>
2023-10-23 16:38:22 -0400

    ui

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -34,8 +34,10 @@ const Home = () => {
                 </div>
                 <div className='flex flex-row flex-wrap items-center justify-center p-4 pt-12'>
                     <div className='flex flex-col items-center justify-center p-4'>
-                        <p>Wednesday-Saturday - 12pm to 9:30pm</p>
-                        <p>Sunday - 12pm to 6:30pm</p>
+                        <p>Wednesday-Saturday - </p>
+                        <p>12pm to 9:30pm</p>
+                        <p>Sunday - </p>
+                        <p>12pm to 6:30pm</p>
                     </div>
                     <div className='p-4'>
                         <p><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8899.430537341206!2d-61.615180559016316!3d12.131878537235167!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8c381771cc8005d1%3A0x4e9dec3d74ad7b8f!2sDre%20Flavors%20Restaurant%20%26%20Catering%20Services!5e0!3m2!1sen!2snl!4v1697746276349!5m2!1sen!2snl" width="400" height="250" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe></p>
