3bf064013fec7690e6f529d59e6780ffdefa398a
kirran01 <Kirran-@live.com>
2023-11-16 15:13:28 -0400

    same

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
     const navigate = useNavigate();
+    const [isLoading, setIsLoading] = useState(false);
     const { storeToken, authenticateUser } = useContext(AuthContext)
     const [errorMessage, setErrorMessage] = useState('')
     const [loginInput, setLoginInput] = useState({
@@ -14,27 +15,33 @@ const Login = () => {
     const handleLoginInput = (e) => {
         setLoginInput({ ...loginInput, [e.target.name]: e.target.value })
     }
-    const login = (e) => {
-        e.preventDefault()
-        axios.post(`${import.meta.env.VITE_API_URL}/auth/login`, {
-            username: loginInput.loginName,
-            password: loginInput.loginPassword
-        })
-            .then(res => {
-                storeToken(res.data.authToken)
-                authenticateUser()
-                navigate('/')
-            })
-            .catch(err => {
-                console.log(err)
-                let theError = err.response.data.message
-                if (theError) {
-                    setErrorMessage(theError)
-                }
-            })
-    }
+    const login = async (e) => {
+        e.preventDefault();
+        setIsLoading(true);
+        try {
+            const response = await axios.post(`${import.meta.env.VITE_API_URL}/auth/login`, {
+                username: loginInput.loginName,
+                password: loginInput.loginPassword
+            });
+            storeToken(response.data.authToken);
+            authenticateUser();
+            navigate('/');
+        } catch (err) {
+            console.log(err);
+            let theError = err.response?.data?.message;
+            if (theError) {
+                setErrorMessage(theError);
+            }
+        } finally {
+            setIsLoading(false);
+        }
+    };
     return (
         <div className='h-screen flex flex-col justify-evenly content-center items-center bg-cyan-50'>
+            {isLoading &&
+                <div>
+                    <p>Loading...</p>
+                </div>}
             {errorMessage !== '' && <div>
                 <p>{errorMessage}</p>
             </div>}
@@ -44,7 +51,7 @@ const Login = () => {
                     <label className='m-2'>Username</label>
                     <input className='border-gray border-2 rounded-md m-2' onChange={handleLoginInput} value={loginInput.loginName} name="loginName" type="text" />
                     <label className='m-2'>Password</label>
-                    <input className='border-gray border-2 rounded-md m-2' onChange={handleLoginInput} value={loginInput.loginPassword} name="loginPassword" type="text" />
+                    <input className='border-gray border-2 rounded-md m-2' onChange={handleLoginInput} value={loginInput.loginPassword} name="loginPassword" type="password" />
                     <button className='bg-cyan-800 hover:bg-cyan-700 p-2 m-5 text-white rounded-md' onClick={login} >Log In</button>
                 </form>
             </div>
